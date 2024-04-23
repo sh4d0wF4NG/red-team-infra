3d9c29e19223f5dc4955eae7ab800be6ce7c368d
sh4d0wF4NG <sh4d0wF4NG@protonmail.com>
2024-04-23 22:50:38 +0800

    Fixed issues with evilginx script

--- ec2_evilginx.tf
+++ ec2_evilginx.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "evilginx_instance" {
   ami                         = data.aws_ami.ubuntu.image_id
   instance_type               = "t2.micro"
   associate_public_ip_address = true
+  subnet_id              = aws_subnet.main_subnet.id
   vpc_security_group_ids = [aws_security_group.ssh_port.id]
   user_data              = <<EOF
 #!/bin/bash
