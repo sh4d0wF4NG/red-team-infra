8f215ebca00ddf3d226c979fcb0d78c3b0add31b
sh4d0wF4NG <sh4d0wF4NG@protonmail.com>
2024-04-23 22:48:52 +0800

    Created new script - evilginx

---
+++ ec2_evilginx.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "evilginx_instance" {
+  ami                         = data.aws_ami.ubuntu.image_id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.ssh_port.id]
+  user_data              = <<EOF
+#!/bin/bash
+cd /tmp
+apt install -y git gcc net-tools
+wget https://go.dev/dl/go1.19.13.linux-amd64.tar.gz -O /opt/go1.19.13.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf /opt/go1.19.13.linux-amd64.tar.gz
+  EOF
+  tags = {
+    Name = "evilginx_instance"
+  }
+}
